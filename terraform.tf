a9f1e3054be1c3ee1f1b3ad3d9dc782f984ebbf2
motte <motte@netcompany.com>
2026-05-05 21:09:03 +0200

    azapi provider

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,10 @@
 terraform {
   required_version = ">=1.3.7"
   required_providers {
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 2.4"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "4.71.0"
