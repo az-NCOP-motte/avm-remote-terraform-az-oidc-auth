07ed269271c68bb3d662c9b6ece42ea361295ab2
motte <motte@netcompany.com>
2026-05-08 11:31:33 +0200

    variables for default example

--- examples/default/variables.tf
+++ examples/default/variables.tf
@@ -7,3 +7,28 @@ For more information see <https://aka.ms/avm/telemetryinfo>.
 If it is set to false, then no telemetry will be collected.
 DESCRIPTION
 }
+
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group provided by tfvars file."
+  default     = "avm-remote-terraform-az-oidc-auth"
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "Subscription ID of the resource group provided by tfvars file."
+  default     = "xxx"
+}
+
+variable "devops_principle_client_id" {
+  type        = string
+  description = "Subscription ID of the resource group provided by tfvars file."
+  default     = "xxx"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region where the resource should be deployed."
+  nullable    = false
+  default     = "West Europe"
+}
