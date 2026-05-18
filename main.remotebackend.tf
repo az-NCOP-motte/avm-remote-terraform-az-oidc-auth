f51b0b1c3ecfcbe48a745f297c3e9762b59a04b0
motte <motte@netcompany.com>
2026-05-18 21:58:26 +0200

    naming for backend blocks

--- main.remotebackend.tf
+++ main.remotebackend.tf
@@ -1,7 +1,7 @@
 #This file is to implement remote backend resource blocks for tfvars
 
 resource "azurerm_storage_account" "tf_state" {
-  name                            = "${lower(var.environment_name)}rgmotte"
+  name                            = "${lower(var.environment_name)}-motte-pipeline"
   resource_group_name             = azurerm_resource_group.TODO.name
   location                        = azurerm_resource_group.TODO.location
   account_tier                    = "Standard"
@@ -12,7 +12,7 @@ resource "azurerm_storage_account" "tf_state" {
 
 # Storage Container
 resource "azurerm_storage_container" "tf_state" {
-  name                  = "${lower(var.environment_name)}tfstate"
+  name                  = "tfstate"
   storage_account_id    = azurerm_storage_account.tf_state.id
   container_access_type = "private"
 }
