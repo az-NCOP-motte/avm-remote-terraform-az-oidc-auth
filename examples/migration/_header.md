2f3d503b72f269a8779998927ee5738fead87337
motte <motte@netcompany.com>
2026-06-08 11:07:41 +0200

    add example for module without remote backend

---
+++ examples/migration/_header.md
@@ -0,0 +1,3 @@
+# Migration example
+
+This deploys the module in with remote backend disabled for first time migration.
---
+++ examples/migration/outputs.tf
@@ -0,0 +1,27 @@
+output "azurerm_client_config_object_id" {
+  value = module.test.azurerm_client_config_object_id
+}
+
+output "azurerm_client_config_subscription_id" {
+  value = module.test.azurerm_client_config_subscription_id
+}
+
+output "azuread_service_principal_clientID" {
+  value = module.test.azuread_service_principal_clientID
+}
+
+output "BACKEND_AZURE_STORAGE_ACCOUNT_CONTAINER_NAME" {
+  value = module.test.BACKEND_AZURE_STORAGE_ACCOUNT_CONTAINER_NAME
+}
+
+output "BACKEND_AZURE_STORAGE_ACCOUNT_NAME" {
+  value = module.test.BACKEND_AZURE_STORAGE_ACCOUNT_NAME
+}
+
+output "BACKEND_RESOURCE_GROUP_NAME" {
+  value = module.test.BACKEND_RESOURCE_GROUP_NAME
+}
+
+output "APP_CONFIG_ENDPOINT" {
+  value = module.test.APP_CONFIG_ENDPOINT
+}
