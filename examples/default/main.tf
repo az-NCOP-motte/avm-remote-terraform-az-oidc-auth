447ff450ef6f402552f32aa889c71d57af2db0f3
motte <motte@netcompany.com>
2026-06-18 12:57:24 +0200

    revent deletion option template

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -29,7 +29,9 @@ terraform {
 
 provider "azurerm" {
   features {
-
+    # resource_group {
+    #   prevent_deletion_if_contains_resources = true
+    # }
   }
   skip_provider_registration = true
   storage_use_azuread        = true
