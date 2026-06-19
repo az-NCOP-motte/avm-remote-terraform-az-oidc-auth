67ed11126459f125e7e70de93d2b87dc6f8d110b
M. Ottenhof <max@ottenhof.cloud>
2026-06-19 19:05:28 +0200

    Refactor  App Configuration with Azure/avm-res-appconfiguration-configurationstore/azure Added purge_protection variable

--- main.remoteconfiguration.tf
+++ main.remoteconfiguration.tf
@@ -56,6 +56,55 @@ module "key_vault" {
   }
 }
 
+# app config and key values
+module "avm-res-appconfiguration-configurationstore" {
+  source  = "Azure/avm-res-appconfiguration-configurationstore/azure"
+  version = "0.5.1"
+
+  location                      = module.az-environment-resourcegroup.location
+  name                          = "${lower(var.environment_name)}-pipeline"
+  resource_group_resource_id    = module.az-environment-resourcegroup.resource_id
+  public_network_access_enabled = true
+  sku                           = "standard"
+  purge_protection_enabled      = var.purge_protection_enabled
+  soft_delete_retention_days    = 3
+
+  key_values = {
+    my_secret_reference = {
+      key          = "MySecretPrinciple"
+      content_type = "application/vnd.microsoft.appconfig.keyvaultref+json;charset=utf-8"
+      value = jsonencode({
+        uri = join("/", slice(split("/", module.key_vault.secrets.principle.id), 0, 5))
+      })
+
+    }
+    resource_group_name = {
+      key   = "resource_group_name"
+      value = module.az-environment-resourcegroup.name
+    }
+    enable_telemetry = {
+      key   = "enable_telemetry"
+      value = var.enable_telemetry
+    }
+    subscription_id = {
+      key   = "subscription_id"
+      value = var.subscription_id
+    }
+    location = {
+      key   = "location"
+      value = var.location
+    }
+    devops_organization_url = {
+      key   = "devops_organization_url"
+      value = var.devops_organization_url
+    }
+    environment_name = {
+      key   = "environment_name"
+      value = var.environment_name
+    }
+    devops_principle_client_id = {
+      key   = "devops_principle_client_id"
+      value = var.devops_principle_client_id
     }
   }
 }
