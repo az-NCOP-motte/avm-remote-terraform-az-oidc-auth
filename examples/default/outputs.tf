b4b867c69bf63e50340b5722982c717c6188571c
M. Ottenhof <max@ottenhof.cloud>
2026-06-21 11:56:02 +0200

    Refactor default folder outputs

--- examples/default/outputs.tf
+++ examples/default/outputs.tf
@@ -1,17 +1,17 @@
-output "azurerm_client_config_object_id" {
-  value = module.test.azurerm_client_config_object_id
+output "azapi_client_config_subscription_id" {
+  value = data.azapi_client_config.current.subscription_id
 }
 
-output "azurerm_client_config_subscription_id" {
-  value = module.test.azurerm_client_config_subscription_id
+output "azapi_client_config_object_id" {
+  value = data.azapi_client_config.current.tenant_id
 }
 
-output "azuread_service_principal_clientID" {
-  value = module.test.azuread_service_principal_clientID
+output "azuread_service_principal_client_id" {
+  value = data.azuread_service_principal.devops_sp.client_id
 }
 
-output "BACKEND_AZURE_STORAGE_ACCOUNT_CONTAINER_NAME" {
-  value = module.test.BACKEND_AZURE_STORAGE_ACCOUNT_CONTAINER_NAME
+output "BACKEND_AZURE_STORAGE_CONTAINER_NAME" {
+  value = module.test.BACKEND_AZURE_STORAGE_CONTAINER_NAME
 }
 
 output "BACKEND_AZURE_STORAGE_ACCOUNT_NAME" {
