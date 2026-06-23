035cd8dc6170e53f621c54297ae0fe655b411328
M. Ottenhof <max@ottenhof.cloud>
2026-06-23 17:43:16 +0200

    add simple appconfig for tfvars example

---
+++ examples/tfvars_appconfig_simple/outputs.tf
@@ -0,0 +1,32 @@
+output "azapi_client_config_subscription_id" {
+  value = module.this.azapi_client_config_subscription_id
+}
+
+output "azapi_client_config_object_id" {
+  value = module.this.azapi_client_config_object_id
+}
+
+output "azuread_service_principal_client_id" {
+  value = module.this.azuread_service_principal_client_id
+}
+
+output "BACKEND_AZURE_STORAGE_CONTAINER_NAMES" {
+  value = module.this.BACKEND_AZURE_STORAGE_CONTAINER_NAMES
+}
+
+output "BACKEND_AZURE_STORAGE_ACCOUNT_NAMES" {
+  value = module.this.BACKEND_AZURE_STORAGE_ACCOUNT_NAMES
+}
+
+output "BACKEND_RESOURCE_GROUP_NAME" {
+  value = module.this.BACKEND_RESOURCE_GROUP_NAME
+}
+
+output "APP_CONFIG_ENDPOINTS" {
+  value = module.this.APP_CONFIG_ENDPOINTS
+}
+
+
+output "debug_appconfig_key_values" {
+  value = module.this.debug_appconfig_key_values
+}
