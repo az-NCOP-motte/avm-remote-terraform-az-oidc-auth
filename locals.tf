555e9cb4ff3bb60b00324cb67aec814f495f1b69
M. Ottenhof <max@ottenhof.cloud>
2026-06-29 12:58:15 +0200

    add tenant and subscription locals

--- locals.tf
+++ locals.tf
@@ -1,4 +1,6 @@
 # TODO: insert locals here.
 locals {
   role_definition_resource_substring = "/providers/Microsoft.Authorization/roleDefinitions"
+  tenant_id                          = data.azapi_client_config.current.tenant_id
+  subscription_id                    = data.azapi_client_config.current.subscription_id
 }
