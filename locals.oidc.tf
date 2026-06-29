afeaee30506d2a02562d6670adfe75af6fc235a1
M. Ottenhof <max@ottenhof.cloud>
2026-06-29 13:02:39 +0200

    Refactor oidc locals

--- locals.oidc.tf
+++ locals.oidc.tf
@@ -1,4 +1,5 @@
-# The object ID of the service principal to assign roles to.
+# The object ID of the service principal to assign roles to. 
 locals {
-  service_principal_id = data.azuread_service_principal.devops_sp.object_id
+  service_principal_object_id = module.serviceconnections[var.service_connection_key].service_principal_object_id
+  service_principal_client_id = module.serviceconnections[var.service_connection_key].service_principal_client_id
 }
