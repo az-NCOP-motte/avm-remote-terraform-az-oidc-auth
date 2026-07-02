0b54a5608bf7bff160ec6224f5562fa67d1a228a
M. Ottenhof <max@ottenhof.cloud>
2026-07-02 19:57:08 +0200

    reformat service connection module

--- main.serviceconnections.tf
+++ main.serviceconnections.tf
@@ -2,9 +2,9 @@ module "serviceconnections" {
   source   = "./modules/serviceconnection"
   for_each = local.serviceconnections
 
-  name                = each.value.name
-  application_name    = each.value.application_name
-  devops_project_id   = var.devops_project_id
-  subscription_id     = local.subscription_id
-  tenant_id           = local.tenant_id
+  name              = each.value.name
+  application_name  = each.value.application_name
+  devops_project_id = var.devops_project_id
+  subscription_id   = local.subscription_id
+  tenant_id         = local.tenant_id
 }
